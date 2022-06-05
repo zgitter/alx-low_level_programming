97123d9f69880fe74c223d9501959f6226e9a1f3
beimnet <getbeimnet@gmail.com>
2022-06-05 07:55:49 -0700

    RNG with sign checker and betty f

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,12 @@
 /* more headers goes there */
 #include<stdio.h>
 /* betty style doc for function main goes there */
+
+/**
+ * main - Generates a random number and checks and prints if it is positive, negative or zero
+ * @n: The generated number
+ * Return: 0 if the function runs correctly or 1 if not
+ */
 int main(void)
 {
 	int n;
@@ -10,11 +16,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if(n>0)
-		printf("%d : is positive\n",n);
-	else if(n==0)
-		printf("%d : is zero\n",n);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	else
-		printf("%d : is negative\n",n);
+		printf("%d is negative\n", n);
 	return (0);
 }
