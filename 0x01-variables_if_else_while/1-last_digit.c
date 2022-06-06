edd59b60c9bb44ed6d70fd52a6da6d3cdbdafe4a
beimnet <getbeimnet@gmail.com>
2022-06-06 01:40:09 -0700

    the last digit generator commit3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,6 +4,7 @@
 #include<stdio.h>
 /**
  * main - This function generates PRN and tells about
+ * Return: returns 0 if main  runs right otherwise 1
  */
 int main(void)
 {
@@ -15,10 +16,16 @@ int main(void)
 	/* your code goes there */
 	last_digit = n % 10;
 	if (last_digit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);  
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	}	
 	else if ((last_digit < 6) && (last_digit != 0))
+	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
-	else 
+	}
+	else
+	{	
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
 	return (0);
 }
