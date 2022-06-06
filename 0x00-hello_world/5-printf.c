bb19ce4285913cee85f0efde8a76cbbc3e5f4af4
beimnet <getbeimnet@gmail.com>
2022-06-06 10:07:19 -0700

    commit for printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+/**
+ * main - Description. main prints a string
+ * Return: returns 0 when runs right
+ */
+int main(void)
+{       printf("with proper grammar%c but the outcome is a piece of art%c\n", 44, 44);
+	return (0);
+}
