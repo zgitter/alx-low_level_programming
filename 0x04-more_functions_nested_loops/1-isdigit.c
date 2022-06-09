1af59f048b522715446857474728c36bcc7250db
beimnet <getbeimnet@gmail.com>
2022-06-09 13:38:09 -0700

    digit commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if char is a digit
+ *
+ * Return: 1 if upper, else 0
+ * @c: is an integer input parameter
+ */
+int _isdigit(int c)
+{
+	int i;
+	int t;
+
+	i = 48;
+	t = 0;
+
+	while (i < 58)
+		if (c == i)
+		{
+			t = 1;
+			break;
+		}
+		else 
+			i++;
+	return (t);
+}
---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - calls _isdigit to check a char
+ *
+ * Return: returns 0 only on success
+ */
+int main(void)
+{
+    char c;
+
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+    return (0);
+}
