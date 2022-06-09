4dec983e72e21e604279f14d24c5c70b5524d39a
beimnet <getbeimnet@gmail.com>
2022-06-09 01:13:05 -0700

    commit for putchar5

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,13 @@
 #include <stddef.h>
 /**
  * main - Calls __putchar to print string
- * Return - returns 0 on success
+ *
+ * Return: returns 0 on success
  */
 int main(void)
 {
 	char chars[] = "_putchar";
-	size_t a_size = sizeof(chars)/sizeof(chars[0]);
+	size_t a_size = sizeof(chars) / sizeof(chars[0]);
 	int l = a_size;
 	int n;
 
