b06a5daebe5fc2c357cac6779d30914c0744b3b1
beimnet <getbeimnet@gmail.com>
2022-06-10 00:33:06 -0700

    committing a product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - takes two intergers and returns their product
+ * @a: first integer 
+ * @b: second integer
+ * Return: return the axb result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
