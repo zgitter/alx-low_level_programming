6747abcb65813b4902a5f80f2e971f89fae4d2d7
beimnet <getbeimnet@gmail.com>
2022-06-10 10:52:59 -0700

    commit for more number + loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * more_numbers - prints 1-14 more times
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+	char c;
+	int k = 49;
+
+	for (j = 1; j < 11; j++)
+	{
+		for (i = 48; i < 63; i++)
+		{
+			if (i < 58)
+			{
+				c = i;
+			}
+			else
+			{	
+				c = k;
+				_putchar(c);
+				c = i - 10;
+			}
+			_putchar(c);
+				
+		}
+		c = 10;
+		_putchar(c);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,4 +9,5 @@ int mul(int a, int b);
 int _putchar(int c);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 #endif /*MAIN_H */
