0d793a85367a3f396b0d812d2aa83c195a3afd8b
beimnet <getbeimnet@gmail.com>
2022-06-11 12:59:36 -0700

    commit for qe skipped a-z

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - This main generates a - z
+ * Return: returns 0 if main  runs right otherwise 1
+ */
+int main(void)
+{
+	char c;
+
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (!(c == 'e' || c == 'q'))
+		{
+		putchar(c);
+		}
+	}
+	c = 10;
+	putchar(c);
+	return (0);
+}
+
