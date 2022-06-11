6d4758a267d1b74232ca9ae582d7b8dbe3f39f34
beimnet <getbeimnet@gmail.com>
2022-06-11 04:38:23 -0700

    commit alfa betvwith putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - This main generates a - z
+ * Return: returns 0 if main  runs right otherwise 1
+ */
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 97; n < 123; n++)
+	{
+		c = 'n'
+		putchar(c);
+	}
+	putchar('10');
+	return (0);
+}
