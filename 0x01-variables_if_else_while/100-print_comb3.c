919371a7a9436eaca3a457e4e820aab82c383b38
beimnet <getbeimnet@gmail.com>
2022-06-12 08:06:53 -0700

    commit for 1 digit combination print3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,16 +6,24 @@
 int main(void)
 {
 	int c;
+	int b;
 
 	for (c = 0; c < 10; c++)
 	{
 		putchar(c + 48);
-		if (c != 9)
+		for ( b = 0; b < 10; b++)
 		{
-		putchar(44);
-		putchar(32);
+			if ( c != b)
+			{
+				putchar(b + 48);
+				if (!(b == 9 && c == 9))
+				{
+					putchar(44);
+					putchar(32);
+				}
 		}
 	}
+}
 	putchar(10);
 	return (0);
 }
