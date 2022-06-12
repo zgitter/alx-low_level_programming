80f450558f06a5ef01ba77bc828bb81be6a325ce
beimnet <getbeimnet@gmail.com>
2022-06-12 14:28:00 -0700

    commit for last digit printing

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <math.h>
+/**
+ * print_last_digit - prints the last digit of any digit num
+ * @n: the input parameters whose last digit is to be found
+ * Return: returns the last digit
+ */
+int print_last_digit(int n)
+{
+	int ld;
+	
+	ld = n % 10;
+	return (ld);
+}
