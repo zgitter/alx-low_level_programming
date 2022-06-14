70eb6c29a45660201cda12cb24b04f295ba4c7f1
beimnet <getbeimnet@gmail.com>
2022-06-14 16:07:14 -0700

    commit for print just half 100th!

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * puts_half - takes the address of a string and prints its last half
- * @str: points to the string to be printed
+ * puts_half - takes the address of a string and half prints it
+ * @str: points to the string to be half printed
  */
 void puts_half(char *str)
 {
@@ -18,22 +18,21 @@ void puts_half(char *str)
 	if (i % 2 == 0)
 	{
 		n = i / 2;
-
 		while (*(str + n + j) != 0)
 		{
-                _putchar(*(str + j + n));
-                j++;
+			_putchar(*(str + n + j));
+			j++;
 		}
 	}
 	else
 	{
 		n = (i - 1) / 2;
-
 		while (*(str + n + j + 1) != 0)
 		{
-                _putchar(*(str + j + n + 1));
-                j++;
+			_putchar(*(str + n + j + 1));
+			j++;
 		}
 	}
 	_putchar(10);
+
 }
