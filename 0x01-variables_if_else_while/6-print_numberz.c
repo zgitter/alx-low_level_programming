195be64514303b4399ee5f64e1643f719dfadeea
beimnet <getbeimnet@gmail.com>
2022-06-15 10:49:17 -0700

    commit for numz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - This main generates a - z
+ * Return: returns 0 if main  runs right otherwise 1
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	i = 10;
+	putchar(i);
+	return (0);
+}
